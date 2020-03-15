9aa6e078716ce61db35004a8e519d0dc10d44cc5
Dmitry Shuvalov <Dmitry_Shuvalov@mail.ru>
2020-03-15 16:52:35 +0300

    Добавлены Models

---
+++ Personal Quiz/Model/Answer.swift
@@ -0,0 +1,12 @@
+//
+//  Answer.swift
+//  Personal Quiz
+//
+//  Created by qwerty on 15.03.2020.
+//  Copyright © 2020 Dmitry Shuvalov. All rights reserved.
+//
+
+struct Answer {
+    var text: String
+    var type: AnimalType
+}
---
+++ Personal Quiz/Model/ResponseType.swift
@@ -0,0 +1,13 @@
+//
+//  ResponseType.swift
+//  Personal Quiz
+//
+//  Created by qwerty on 15.03.2020.
+//  Copyright © 2020 Dmitry Shuvalov. All rights reserved.
+//
+
+enum ResponseType {
+    case single
+    case multiple
+    case ranged
+}
